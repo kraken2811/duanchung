5ad8461e09d28de2c129155a0bfaea0bdd288239
NotQuangPhuc <doanphucdp31124@gmail.com>
2025-12-23 17:11:35 +0700

    add login page

---
+++ frontend/src/page/Login/api/rule.api.js
@@ -0,0 +1,5 @@
+export const AUTH_RULES = {
+  taxCode: { required: "Vui lòng nhập mã số thuế doanh nghiệp" },
+  username: { required: "Tên đăng nhập không được để trống" },
+  password: { required: "Mật khẩu không được để trống" },
+};
