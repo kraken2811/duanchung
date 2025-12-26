2cef75f0dc0fda74e691522dd869fbcf8b35ed78
kraken2811 <longngungo3124@gmail.com>
2025-12-27 00:13:17 +0700

    fix_value_product

--- frontend/src/page/Product/api/value.api.js
+++ frontend/src/page/Product/api/value.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getValueAdjustments = (params) =>
-  axios.get("/chi_tiet_dieu_chinh_tri_gia", { params });
+  axios.get("/chi_tiet_dieu_chinh_tri_gias", { params });
 
 export const createValueAdjustment = (data) =>
-  axios.post("/chi_tiet_dieu_chinh_tri_gia", data);
+  axios.post("/chi_tiet_dieu_chinh_tri_gias", data);
